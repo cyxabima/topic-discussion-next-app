a7d0953bcac9991128d9272a8230489b445a4f65
cyxabima <ukashaanwerali@gmail.com>
2025-07-20 13:16:26 +0500

    small manupilation in comment create

--- src/components/comments/comment-create-form.tsx
+++ src/components/comments/comment-create-form.tsx
@@ -12,7 +12,7 @@ const CommentCreateForm = ({ postId, parentId, stateOpen }: { postId: string; pa
         <div>
             <Button size={'sm'} variant={'link'} onClick={() => setOpen(!open)}>Reply</Button>
             {open &&
-                <form action={action} className='space-y-2'>
+                <form action={action} className='space-y-2' onSubmit={() => setOpen(false)}>
                     <Textarea
                         placeholder='write a comment...'
                         className='bg-gray-100 focus-visible:ring-0'
